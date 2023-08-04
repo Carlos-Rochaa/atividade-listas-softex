dbd5af699a18291c33d262d8818e713c64727bab
Carlos Rocha <carlosrocha1896@gmail.com>
2023-08-04 11:34:43 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+let nomes = [
+  "Carlos",
+  "João",
+  "Maria",
+  "Pedro",
+  "Marcos",
+  "Lucas",
+  "Felipe",
+  "Bruno",
+  "Julia",
+  "Paula",
+];
+let idades = [20, 30, 23, 24, 26, 27, 28, 31, 18, 21];
+let coresFavoritas = [
+  "azul",
+  "verde",
+  "rosa",
+  "amarelo",
+  "vermelho",
+  "laranja",
+  "roxo",
+  "preto",
+  "branco",
+  "cinza",
+];
+
+console.log(nomes);
+console.log(idades);
+console.log(coresFavoritas);
+
+idades[2] = 33;
+coresFavoritas[5] = "azul-marinho";
+
+console.log(nomes);
+console.log(idades);
+console.log(coresFavoritas);
